008d75a059612f1e87f80435f4203308430835f0
Gabriel Souza <99844838+gbrlszsml@users.noreply.github.com>
2023-02-28 01:49:52 -0300

    Delete Ex 1.js

--- Ex 1/Ex 1.1/Ex 1.js
+++
@@ -1,9 +0,0 @@
-let dado = prompt("Digite um dado:");
-let confirmacao = confirm("Deseja verificar o tipo do dado informado?");
-    if (confirmacao) 
-    {
-        document.write(`O tipo do dado informado é: ${typeof dado}`);
-    }
- else {
-    document.write("Obrigado por visitar esta página");
-}
